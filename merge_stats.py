73fd70a281ac0717f488cdeef049b05f4a50f9af
CodingWithTim <tianle@ray-skyserve-1-head-0e203f0a-compute.c.skypilot-375900.internal>
2023-07-13 03:01:39 +0000

    combine stats across models

---
+++ merge_stats.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import os
+from tqdm import tqdm
+
+models = ["vicuna7b", "vicuna13b"]
+
+def get_subcategories():
+    return {
+        "abstract_algebra": ["math"],
+        "anatomy": ["health"],
+        "astronomy": ["physics"],
+        "business_ethics": ["business"],
+        "clinical_knowledge": ["health"],
+        "college_biology": ["biology"],
+        "college_chemistry": ["chemistry"],
+        "college_computer_science": ["computer science"],
+        "college_mathematics": ["math"],
+        "college_medicine": ["health"],
+        "college_physics": ["physics"],
+        "computer_security": ["computer science"],
+        "conceptual_physics": ["physics"],
+        "econometrics": ["economics"],
+        "electrical_engineering": ["engineering"],
+        "elementary_mathematics": ["math"],
+        "formal_logic": ["philosophy"],
+        "global_facts": ["other"],
+        "high_school_biology": ["biology"],
+        "high_school_chemistry": ["chemistry"],
+        "high_school_computer_science": ["computer science"],
+        "high_school_european_history": ["history"],
+        "high_school_geography": ["geography"],
+        "high_school_government_and_politics": ["politics"],
+        "high_school_macroeconomics": ["economics"],
+        "high_school_mathematics": ["math"],
+        "high_school_microeconomics": ["economics"],
+        "high_school_physics": ["physics"],
+        "high_school_psychology": ["psychology"],
+        "high_school_statistics": ["math"],
+        "high_school_us_history": ["history"],
+        "high_school_world_history": ["history"],
+        "human_aging": ["health"],
+        "human_sexuality": ["culture"],
+        "international_law": ["law"],
+        "jurisprudence": ["law"],
+        "logical_fallacies": ["philosophy"],
+        "machine_learning": ["computer science"],
+        "management": ["business"],
+        "marketing": ["business"],
+        "medical_genetics": ["health"],
+        "miscellaneous": ["other"],
+        "moral_disputes": ["philosophy"],
+        "moral_scenarios": ["philosophy"],
+        "nutrition": ["health"],
+        "philosophy": ["philosophy"],
+        "prehistory": ["history"],
+        "professional_accounting": ["other"],
+        "professional_law": ["law"],
+        "professional_medicine": ["health"],
+        "professional_psychology": ["psychology"],
+        "public_relations": ["politics"],
+        "security_studies": ["politics"],
+        "sociology": ["culture"],
+        "us_foreign_policy": ["politics"],
+        "virology": ["health"],
+        "world_religions": ["philosophy"],
+    }
+
+
+def get_categories():
+    return {
+        "STEM": [
+            "physics",
+            "chemistry",
+            "biology",
+            "computer science",
+            "math",
+            "engineering",
+        ],
+        "humanities": ["history", "philosophy", "law"],
+        "social sciences": [
+            "politics",
+            "culture",
+            "economics",
+            "geography",
+            "psychology",
+        ],
+        "other (business, health, misc.)": ["other", "business", "health"],
+    }
+
+subjects = sorted(
+    [
+        f.split("_test.csv")[0]
+        for f in os.listdir(os.path.join("data", "mmlu", "test"))
+        if "_test.csv" in f
+    ]
+)
+
+    
+for subject in tqdm(subjects):
+
+    test_df = pd.read_csv(
+        os.path.join("data", "mmlu", "test", subject + "_test.csv"), header=None
+    )
+
+    test_df.columns = ["Questions", "Choice 1", "Choice 2", "Choice 3", "Choice 4", "Correct Answer"]
+
+    for model in models:
+        tail = "_" + model +".csv"
+
+        model_df = pd.read_csv(
+            os.path.join(model, subject + tail), header=None
+        )
+
+        test_df[model + " Answer"] = model_df.iloc[model_df.shape[1] - 1]
+
+    test_df.to_csv(
+        os.path.join("combined", subject + ".csv"), index=False, header=True
+    )
